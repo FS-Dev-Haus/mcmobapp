bb63c80167d345cded86d9eec6d8726358867567
Iqbal Bin Mohamad Zainuddin <iqbalz.dev@gmail.com>
2021-06-30 21:56:32 +0800

    Design login page

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -8,10 +8,121 @@ class LoginForm extends StatefulWidget {
 }
 
 class _LoginFormState extends State<LoginForm> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  submitLogin() {
+
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            margin: EdgeInsets.only(left: 50, right: 50, bottom: 20),
+            child: Image.asset('assets/img/MClogo.png'),
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 50, right: 50),
+            child: TextField(
+              controller: emailController,
+              textAlign: TextAlign.center,
+              cursorHeight: 25,
+              cursorColor: Colors.black,
+              style: TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.w300,
+              ),
+              decoration: InputDecoration(
+                alignLabelWithHint: true,
+                labelStyle: TextStyle(
+                  color: Colors.grey 
+                ),
+                labelText: "Your Email",
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black)
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black)
+                ),
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 50, right: 50),
+            child: TextField(
+              controller: passwordController,
+              obscureText: true,
+              textAlign: TextAlign.center,
+              cursorHeight: 25,
+              cursorColor: Colors.black,
+              style: TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.w300,
+              ),
+              decoration: InputDecoration(
+                alignLabelWithHint: true,
+                labelStyle: TextStyle(
+                  color: Colors.grey,
+                ),
+                labelText: "Password",
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black)
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black)
+                ),
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 50, right: 50, top: 30),
+            child: ConstrainedBox(
+              constraints: BoxConstraints.tightFor(width: 500),
+              child: ElevatedButton.icon(
+                onPressed: (){}, 
+                label: Text('Login'),
+                icon: Icon(Icons.login), 
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.blueAccent,
+                  shadowColor: Colors.grey,
+                  shape: new RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)
+                  ),
+                  textStyle: TextStyle(
+                    fontWeight: FontWeight.w300
+                  ),
+                ),
+              ),
+            )
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 50, right: 50),
+            child: ConstrainedBox(
+              constraints: BoxConstraints.tightFor(width: 500),
+              child: ElevatedButton.icon(
+                onPressed: (){}, 
+                label: Text('Not Registered Yet?'),
+                icon: Icon(Icons.app_registration), 
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.black,
+                  shadowColor: Colors.grey,
+                  shape: new RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)
+                  ),
+                  textStyle: TextStyle(
+                    fontWeight: FontWeight.w300
+                  ),
+                ),
+              ),
+            )
+          ),
+        ],
+      ),
     );
   }
 }
