c650164b4725612c772e96a95a3a2c53380f4bf0
aida <aidazamziela@gmail.com>
2021-07-26 17:56:53 +0800

    add category page

--- lib/pages/dashboard/addCategory.dart
+++ lib/pages/dashboard/addCategory.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class addCategoryPage extends StatefulWidget {
-  const addCategoryPage({ Key? key }) : super(key: key);
+  const addCategoryPage({Key? key}) : super(key: key);
 
   @override
   _addCategoryPageState createState() => _addCategoryPageState();
@@ -11,8 +11,26 @@ class _addCategoryPageState extends State<addCategoryPage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(
-        child: Text('Add Category'),
+      padding: EdgeInsets.all(20),
+      child: Column(
+        children: [
+          TextField(
+            textAlign: TextAlign.left,
+            decoration: InputDecoration(
+              hintText: 'Name',
+              contentPadding: EdgeInsets.all(20.0),
+            ),
+          ),
+          //TextField(
+          //contentPadding: EdgeInsets.all(20),
+          //decoration: InputDecoration(
+          // border: UnderlineInputBorder(), labelText: 'Name'),
+          // ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text("Add Category"),
+          )
+        ],
       ),
     );
   }
